937eba4489573a36b49a9214fe73222c8b1d1938
castrumai <castrumai@gmail.com>
2025-11-21 05:15:09 -0500

    Task 2.4 - finalized refactoring

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -4,6 +4,9 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * Class for printing statement.
+ */
 public class StatementPrinter {
     private Invoice invoice;
     private Map<String, Play> plays;
@@ -21,28 +24,46 @@ public class StatementPrinter {
         return plays;
     }
 
+    /**
+     * Method for printing the statement.
+     * @return returns string representation of invoice
+     */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
-        StringBuilder result =
+
+        final int volumeCredits = getTotalVolumeCredits();
+
+        final int totalAmount = getTotalAmount();
+
+        final StringBuilder result =
                 new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
-
+        
         for (Performance performance : invoice.getPerformances()) {
-
-            volumeCredits += getVolumeCredits(performance);
-
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
                     usd(getAmount(performance)),
                     performance.getAudience()));
-
-            totalAmount += getAmount(performance);
         }
 
         result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getAmount(performance);
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getVolumeCredits(performance);
+        }
+        return result;
     }
 
     private String usd(int amountInCents) {
